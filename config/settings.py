d93262b15289c66876a17655f3c29e820aa9b86a
Andy Mc <andymcc77@blueyonder.co.uk>
2021-05-08 11:25:29 +0100

    ch12

--- config/settings.py
+++ config/settings.py
@@ -183,3 +183,6 @@ EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
