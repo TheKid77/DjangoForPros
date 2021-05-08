fe9faad8d18ed9f200f2a228474a5fa4c71a43b6
Andy Mc <andymcc77@blueyonder.co.uk>
2021-05-08 08:02:34 +0100

    ch11

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -12,8 +13,25 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     
+    
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
+
+class Review(models.Model):
+    
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    
+    def __str__(self):
+        return self.review
